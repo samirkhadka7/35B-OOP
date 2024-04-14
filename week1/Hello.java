846dbe284877bf494e5c9bdad566278de2425699
Samir <khadkasamir8079@gmail.com>
2024-04-14 14:34:27 +0545

    task completed

--- week1/Hello.java
+++ week1/Hello.java
@@ -1,8 +1,47 @@
 package week1;
 public class Hello {
     public static void main(String[] args) {
+
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello world!");
+
+        /*2. Extend/Modify the above program to print two additional lines:*/
+
+        System.out.print("This is my first program\n");
+        System.out.print("I am on module ST4003CEM\n");
+
+/*4. WAP that prints the following pattern:
+
+ */
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+
+
+
+        /*5. WAP prints the following pattern:*/
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+     
+        
+        /*6. WAP that prints the following output: */
+        System.out.println("A well-formed Java program has a main method with { and } braces.");
+        System.out.println("");
+        System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println("");
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?");
+        System.out.println("");
+        System.out.println("One is what we see when we're typing our program. The other is what appears on the \"console.\"");
+    
+
     }
 
     
 }
+
